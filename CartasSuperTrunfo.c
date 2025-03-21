76b786fd19927141a48032710e6ca7502ec7cc84
LeoJss <leo.jss@outlook.com>
2025-03-21 15:22:37 -0300

    inserindo comentários

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
  
+//Declaração de todas as variáveis
+ 
 int main() {
 
     char estado1, estado2;
@@ -16,37 +18,51 @@ int main() {
 
     float area2, pib2;
  
-    // Exibir apenas o texto "Carta 1:"
+    // Exibi o label "Carta 1:"
 
     printf("Carta 1:\n");
+ 
+    //Solicita e lê a entrada do código do Estado da primeira carta
 
     printf("Digite o estado (A até H): ");
 
     scanf(" %c", &estado1);
 
     getchar(); // Limpar buffer do teclado
+ 
+    //Solicita e lê a entrada do código da primeira carta
 
-    printf("Digite o código: ");
+    printf("Digite o código da Carta: ");
 
     scanf(" %s", codigo1);
 
     getchar(); // Limpar buffer do teclado
+ 
+    //Solicita e lê a entrada da Cidade da primeira carta
 
     printf("Digite o nome da cidade: ");
 
     fgets(nomeCidade1, sizeof(nomeCidade1), stdin);
+ 
+    //Solicita e lê a entrada da população da primeira carta
 
     printf("Digite a população: ");
 
     scanf(" %d", &populacao1);
+ 
+    //Solicita e lê a entrada da área total da primeira carta
 
     printf("Digite a área: ");
 
     scanf(" %f", &area1);
+ 
+    //Solicita e lê a entrada do PIB da primeira carta
 
     printf("Digite o PIB: ");
 
     scanf(" %f", &pib1);
+ 
+    //Solicita e lê o número de pontos turísticos da primeira carta
 
     printf("Digite o número de pontos turísticos: ");
 
@@ -54,45 +70,59 @@ int main() {
 
     getchar(); // Limpar buffer do teclado
 
-    // Exibir apenas o texto "Carta 2:"
+    // Exibi o label "Carta 2:"
 
     printf("\nCarta 2:\n");
+ 
+    //Solicita e lê a entrada do código do Estado da segunda carta
 
     printf("Digite o estado (A até H): ");
 
     scanf(" %c", &estado2);
 
     getchar(); // Limpar buffer do teclado
+ 
+    //Solicita e lê a entrada do código da segunda carta
 
     printf("Digite o código: ");
 
     scanf(" %s", codigo2);
 
     getchar(); // Limpar buffer do teclado
+ 
+    //Solicita e lê a entrada da Cidade da segunda carta
 
     printf("Digite o nome da cidade: ");
 
     fgets(nomeCidade2, sizeof(nomeCidade2), stdin);
+ 
+    //Solicita e lê a entrada da população da segunda carta
 
     printf("Digite a população: ");
 
     scanf(" %d", &populacao2);
+ 
+    //Solicita e lê a entrada da área total da primeira carta
 
     printf("Digite a área: ");
 
     scanf(" %f", &area2);
+ 
+    //Solicita e lê a entrada do PIB da segunda carta
 
     printf("Digite o PIB: ");
 
     scanf(" %f", &pib2);
+ 
+    //Solicita e lê o número de pontos turísticos da segunda carta
 
     printf("Digite o número de pontos turísticos: ");
 
     scanf(" %d", &numPontosTuristicos2);
 
-    // Exibindo os dados coletados
+    // Exibindo os dados coletados das duas cartas
 
-    printf("\n=== Informações das Cartas ===\n");
+    printf("\n=== Informações Coletadas ===\n");
 
     printf("Carta 1:\n");
 
